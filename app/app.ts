551f338f359f805a312387d7bbfbb7ad61019070
Abid Hasan <abid.northsouth@gmail.com>
2021-01-30 11:14:32 +0600

    Add gzip compressions

--- app/app.ts
+++ app/app.ts
@@ -5,10 +5,12 @@ import {authorRouter} from "./route/author";
 import {bookLoanRouter} from "./route/book_loan";
 import {userRouter} from "./route/user";
 import {authenticateToken} from "./middleware/auth";
+import compression from "compression";
 
 const app = express();
 
 app.use(express.json());
+app.use(compression())
 
 //All api endpoints and router configuration
 app.use("/books", authenticateToken, bookRouter);
